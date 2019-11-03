c3aaf4e761f085959a16bdd6b68b38f08c7868e0
Eudes-batista <eudeskyo@hotmail.com>
2019-11-03 02:25:30 +0000

    Resolvido problemas tipo de frete service

--- src/app/servicos/tipo-frete.service.ts
+++ src/app/servicos/tipo-frete.service.ts
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { TipoFrete } from '../modelo/tipoFrete.class';
-import { GenericService } from './generic.service';
-
-@Injectable()
-export class TipoFreteService extends GenericService<TipoFrete>{
-=======
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -16,17 +6,12 @@ import { TipoFrete } from '../modelo/tipoFrete.class';
 
 @Injectable()
 export class TipoFreteService extends GenericService<TipoFrete> {
->>>>>>> 8633cb9bdc756ccbd628610333aed29fb4c2835b
 
   constructor(public httpClient: HttpClient) {
     super(httpClient);
   }
-<<<<<<< HEAD
-   public getResource(): string {
-=======
 
   public getResource(): string {
->>>>>>> 8633cb9bdc756ccbd628610333aed29fb4c2835b
     return 'tipoFrete';
   }
 }

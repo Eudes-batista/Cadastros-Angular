9e57f10b39ffc5f1a6b20418cf926c4ad81901b4
Eudes-batista <eudeskyo@hotmail.com>
2019-11-03 02:38:04 +0000

    Criado componente de tipo de frete

---
+++ src/app/cadastro/tipo-frete/tipo-frete-cadastro/tipo-frete-cadastro.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
+
+import { Componente } from 'src/app/components/componente.class';
+import { TipoFrete } from 'src/app/modelo/tipoFrete.class';
+import { TipoFreteService } from 'src/app/servicos/tipo-frete.service';
+
+
+
+@Component({
+  selector: 'app-tipo-frete-cadastro',
+  templateUrl: './tipo-frete-cadastro.component.html',
+  styleUrls: ['./tipo-frete-cadastro.component.css']
+})
+export class TipoFreteCadastroComponent extends Componente<TipoFrete> implements OnInit {
+  constructor(
+      private tipoFreteService: TipoFreteService
+    , public confirmationService: ConfirmationService
+    , public messageService: MessageService
+    , private route: ActivatedRoute
+  ) {
+    super(tipoFreteService, confirmationService, messageService);
+  }
+
+  ngOnInit() {
+    this.buscarEntidade(this.route, new TipoFrete());
+  }
+
+  ngOnDestroy() {
+    this.inscricaoAlterar$.unsubscribe();
+  }
+}
